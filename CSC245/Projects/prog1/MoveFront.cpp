a8c9f3754b76d76bffd35929df984fa8e9779028
Ronald Karamuca <11005050@live.mercer.edu>
2020-09-15 21:43:06 -0400

    Added functions to client file

--- CSC245/Projects/prog1/MoveFront.cpp
+++ CSC245/Projects/prog1/MoveFront.cpp
@@ -2,31 +2,90 @@
 #include "dlist.h"
 using namespace std;
 
+void HeadInsert( DList<int>& list );
+void TailInsert( DList<int>& list );
+void AccessItem( DList<int>& list );
+void FindandMove( DList<int>& list, int item );
+void Delete( DList<int>& list );
+void PrintList( DList<int>& list );
+void PrintLength( DList<int>& list );
+
 int main()
 {
 	DList<int> list;
-	list.insertHead(10);
-	list.appendTail(15);
-	list.appendTail(20);
-	list.appendTail(25);
-	cout << "First list print:\n";
-	list.print();
-	
-	list.deleteItem(10);
-	cout << "List after head removed:\n";
-	list.print();
-	
-	list.deleteItem(25);
-	cout << "List after tail removed:\n";
-	list.print();
 
-	list.deleteItem(15);
-	cout << "List after tail removed:\n";
-	list.print();
-	
-	list.deleteItem(20);
-	cout << "List after last item removed:\n";
-	list.print();
-
+	HeadInsert(list);
+	PrintList(list);
+	Delete(list);
+	PrintList(list);
 	return 0;
+}
+
+void HeadInsert( DList<int>& list ) 
+{
+	cout << "Enter the new head: ";
+	int item;
+	cin >> item;
+	if( list.inList( item ) ) 
+		cout << "Sorry, that value is already in the list.\n" << endl;
+	else
+		list.insertHead(item);
+}
+
+void TailInsert( DList<int>& list )
+{
+	cout << "Enter the new tail: ";
+	int item;
+	cin >> item;
+	
+	if( list.inList( item ) )
+		cout << "Sorry, that value is already in the list.\n" << endl;
+	else 
+		list.appendTail( item );
+}
+
+void AccessItem( DList<int>& list ) 
+{
+	cout << "Enter the value you want to access: ";
+	int item;
+	cin >> item;
+
+	FindandMove( list, item );
+}
+
+void FindandMove( DList<int>& list, int item )
+{
+	if( list.isEmpty() ) 
+		cout << "List is empty.\n";
+	else if( list.inList( item ) ) {
+		int newItem = item;
+		list.insertHead( newItem );
+		cout << "Item: " << newItem << endl;
+		list.deleteItem( item );
+	}
+	else 
+		cout << "Item not in list.\n";
+}
+
+void Delete( DList<int>& list ) 
+{
+	cout << "Enter item to delete: ";
+	int item;
+	cin >> item;
+
+	if( list.isEmpty() ) 
+		cout << "List is empty.\n";
+	else if( list.inList( item ) ) {
+		list.deleteItem( item );
+	}
+	else 
+		cout << "Item not in list.\n";
+}
+
+void PrintList( DList<int>& list ) 
+{
+	if( list.isEmpty() ) 
+		cout << "List is empty.\n";
+	else 
+		list.print();
 }

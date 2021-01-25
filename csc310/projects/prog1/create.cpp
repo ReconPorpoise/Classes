ff46e7892a38a89c7b42fecf05c80b770b7a041b
Ronald Karamuca <11005050@live.mercer.edu>
2021-01-25 00:34:35 -0500

    Added prog1 boilerplate

---
+++ csc310/projects/prog1/create.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef char String[25];
+struct BookRec
+{
+	unsigned int isbn;
+	String name;
+	String author;
+	int onhand;
+	float price;
+	String type;
+}
+
+int main()
+{
+	
+}
